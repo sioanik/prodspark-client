2b6dc5e7a50b71dba42e793a9342f007e8786894
sioanik <sio.anik.007@gmail.com>
2024-08-17 17:53:02 +0600

    nav 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,13 +15,28 @@
 // }
 
 
-/** @type {import('tailwindcss').Config} */
+
+// /** @type {import('tailwindcss').Config} */
+// import daisyui from "daisyui";
+// export default {
+// content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// theme: {
+// extend: {},
+// },
+// plugins: [daisyui],
+// daisyui: { themes: ["light"] },
+// };
+
+
+/* eslint-disable no-undef */
+const withMT = require("@material-tailwind/react/utils/withMT");
 import daisyui from "daisyui";
-export default {
-content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-theme: {
-extend: {},
-},
-plugins: [daisyui],
-daisyui: { themes: ["light"] },
-};
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: { themes: ["light"] },
+});
+
